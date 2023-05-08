e11d12df1dfe5e1ac6bd3e73745d4c519b779839
Stefan Erkenberg <stefan@erkenberg.info>
2023-05-08 20:46:55 +0200

    Improve chart labels

--- app/table/[tableId]/chart.tsx
+++ app/table/[tableId]/chart.tsx
@@ -1,8 +1,9 @@
 import React, { JSX } from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 import { Pie } from 'react-chartjs-2';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend, ChartDataLabels);
 
 interface ChartProps {
   labels: string[];
@@ -11,11 +12,19 @@ interface ChartProps {
 
 export const Chart = (props: ChartProps): JSX.Element => (
   <Pie
+    options={{
+      plugins: {
+        datalabels: {
+          anchor: 'center',
+          formatter: (value, context): string => {
+            return props.labels[context.dataIndex];
+          },
+        },
+      },
+    }}
     data={{
-      labels: props.labels,
       datasets: [
         {
-          label: '# of Votes',
           data: props.data,
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
