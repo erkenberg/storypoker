045643359a2b5aaaccfcd2840400aaf79d58d4b5
Stefan Erkenberg <stefan@erkenberg.info>
2023-12-19 20:42:13 +0100

    (Temporarily) Disable strict mode to fix rendering errors in dev mode

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Strict mode leads to errors in the `app/table/[tablename]/client-page.tsx` useeffects for subscribing to the
+  // supabase channels when executing the `useEffect`s twice in dev mode.
+  // TODO: figure out to correctly setup the `useEffect`s and enable strict mode again
+  // (lol, as if this will ever happen)
+  reactStrictMode: false,
+};
 
 module.exports = nextConfig;
