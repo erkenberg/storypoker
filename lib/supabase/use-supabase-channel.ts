c4a7067c494c30b0b4a273cc05ec061ba5535bc2
Stefan Erkenberg <stefan@erkenberg.info>
2023-12-19 20:59:28 +0100

    Create supabase Client only once for all channels, to prevent console warnings

--- lib/supabase/use-supabase-channel.ts
+++ lib/supabase/use-supabase-channel.ts
@@ -2,32 +2,28 @@ import { useMemo } from 'react';
 import { createClient, RealtimeChannel } from '@supabase/supabase-js';
 import { useClientId } from '@/lib/use-client-id';
 
+const client = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
+  {
+    realtime: {
+      params: {
+        eventsPerSecond: 10,
+      },
+    },
+  },
+);
+
 export const useSupabaseChannel = (
   channelId: string,
 ): [RealtimeChannel, () => void] => {
   const clientId = useClientId();
-  const client = useMemo(() => {
-    return createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '',
-      {
-        realtime: {
-          params: {
-            eventsPerSecond: 10,
-          },
-        },
-      },
-    );
-  }, []);
   const channel = useMemo(() => {
     return client.channel(channelId, {
       config: { presence: { key: clientId } },
     });
-  }, [client, channelId, clientId]);
+  }, [channelId, clientId]);
 
-  const cleanup = useMemo(
-    () => () => client.removeChannel(channel),
-    [client, channel],
-  );
+  const cleanup = useMemo(() => () => client.removeChannel(channel), [channel]);
   return [channel, cleanup];
 };
