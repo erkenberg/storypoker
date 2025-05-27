fa1558c3c644e7ef3722033ee72d58b912409085
Stefan Erkenberg <stefan.erkenberg@mobilex.eu>
2025-05-27 10:46:33 +0200

    Add supabase database TypeScript types

--- lib/supabase/create-server-client.ts
+++ lib/supabase/create-server-client.ts
@@ -1,9 +1,12 @@
 'use server';
 
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { Database } from '@/database.types';
 
-export const createServerClient = async (): Promise<SupabaseClient> => {
-  return createClient(
+export const createServerClient = async (): Promise<
+  SupabaseClient<Database>
+> => {
+  return createClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL ?? '',
     process.env.SUPABASE_SERVICE_ROLE_KEY ?? '',
     { auth: { persistSession: false } },
